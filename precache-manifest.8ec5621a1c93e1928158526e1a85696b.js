6382d015be38f45b22f342e579dc7d61da02be72
Aleksndar Kenderesi <alek.kenderesi@gmail.com>
2020-09-11 10:11:03 +0200

    Updates

---
+++ precache-manifest.8ec5621a1c93e1928158526e1a85696b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98e0cca6bceba0fe5106d9766de2b12",
+    "url": "/type/index.html"
+  },
+  {
+    "revision": "0102e646804c5ae5f978",
+    "url": "/type/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/type/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/type/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0102e646804c5ae5f978",
+    "url": "/type/static/js/main.00d25105.chunk.js"
+  },
+  {
+    "revision": "324486d604c48a493a5c",
+    "url": "/type/static/js/runtime-main.84367788.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/type/static/media/logo.5d5d9eef.svg"
+  }
+]);
